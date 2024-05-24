5ceb8629d96959e08a5c4da6eea5aac8359ff949
Iverz <140925089+asterixiverz@users.noreply.github.com>
2024-05-24 16:57:14 +0700

    vendorsetup: Add kernel source command

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,3 +6,7 @@ git clone https://github.com/inexdroid/hardware_qcom-caf_msm8998_display hardwar
 
 rm -rf hardware/qcom-caf/msm8998/media
 git clone https://github.com/inexdroid/hardware_qcom-caf_msm8998_media hardware/qcom-caf/msm8998/media
+
+rm -rf kernel/asus/sdm660
+git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b 14 kernel/asus/sdm660
+rm -rf kernel/asus/sdm660/KernelSU/userspace
